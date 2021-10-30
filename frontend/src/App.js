44fd7de029137352662aca211da5fb27e4371275
Anton Nikiforov <nikiforov@csus.edu>
2021-10-30 16:33:05 -0700

    Merged in changes and updated dirs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+<<<<<<< Updated upstream
 import PathfindingVisualizer from './components/PathfindingVisualizer';
 import Header from './components/Header/Header'
+=======
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import Header from './Header/Header'
+
+>>>>>>> Stashed changes
 function App(){
   return (
     <div className="App">
-      <Header></Header>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
