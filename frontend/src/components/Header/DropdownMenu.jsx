fedfa1ce821ec0ae439c568725cf9a4d4419a04b
Anton Nikiforov <nikiforov@csus.edu>
2021-10-31 10:52:53 -0700

    Updated PV after learning more aobut the .map function

--- frontend/src/components/Header/DropdownMenu.jsx
+++ frontend/src/components/Header/DropdownMenu.jsx
@@ -1,4 +1,3 @@
-import React, {useState} from "react";
 import '../../styles/components/dropDownMenu.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
--- frontend/src/components/PathfindingVisualizer.jsx
+++ frontend/src/components/PathfindingVisualizer.jsx
@@ -4,93 +4,87 @@ import '../styles/components/grid.css'
 import '../styles/components/node.css'
 import '../styles/layout/coordinate-text.css'
 
+// main function that builds the whole component
+function PathfindingVisualizer(props) {
+   
+    // Intializing each state variable
+    const [grid, setGrid] = useState([]);
+    const [currRow, setCurrRow] = useState(0);
+    const [currCol, setCurrCol] = useState(0);
+
+    // Initializing each constant variable
+    const ROW_COUNT = 20;
+    const COLUMN_COUNT = 60;
+
+    // Builds and sets the grid value
+    useEffect(() => {
+        const grid = buildInitialGrid();
+        setGrid(grid);
+    }, []);
+
+    // Changes the currRow and currCol when a node button is clicked
+    function handleClick(col, row) {
+        setCurrRow(row);
+        setCurrCol(col);
+    };
+
+
+    // Creats an 2D initial grid object that stores a Node shell object into each (row, col)
+    function buildInitialGrid() {
+        return Array.from(Array(ROW_COUNT)).map((_, rowIndex) => {
+            return Array.from(Array(COLUMN_COUNT)).map((_, columnIndex) => {
+                return createNodeShell(rowIndex, columnIndex)
+            })
+        })
+    };
+
+    // Creates a Node object with the passed in row and col values
+    const createNodeShell = (row, col) => { return { row, col } };
+
+    /*
+        Itterate throuch every column in the row, insert a Node element into each column
+        and then return the results as TableRow elements
+    */
+    const getTableRowElements = (row, rowIdx) => (<tr key={rowIdx}>{row.map(getNode)}</tr>);
+
+    // Generates a Node element
+    const getNode = (node) => {
+        const { row, col } = node;
+        return (
+            <Node
+                key={col}
+                col={row}
+                row={col}
+                onClick={() => handleClick(col, row)}
+            ></Node>
+        )
+    }
+
+    // return elements that displays the Coordinates and the Grid 
+    return (
+        <div>
+            <div className="coordinate">Coordinates: (col, row) ({currCol},{currRow})</div>
+            <table className="grid-container" /* grid is put into a table */ >
+                <tbody className="grid" /* no theader just tbody */>
+                    {
+                        // Itterate through each row and insert a Node element into each column
+                        grid.map(getTableRowElements)
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
 // Node function that returns a button with id, className, and onClick function
-function Node (props) {
-    const {col, row} = props;
+function Node(props) {
+    const { col, row } = props;
     return (
         <button
             id={`node-${row}-${col}`}
             className={`node`}
             onClick={() => props.onClick()}
         ></button>
-    );
-}
-
-// main function that builds the whole component
-function PathfindingVisualizer (props) {
-    // intializing each state variable
-    const [grid, setGrid] = useState([]);
-    const [currRow, setCurrRow] = useState(0);
-    const [currCol, setCurrCol] = useState(0);
-
-    // initializing each constant variable
-    const ROW_COUNT = 20;
-    const COLUMN_COUNT = 60;
-
-    // creats an 2D initial grid object that stores a Node object into each (row, col)
-    function buildInitialGrid (){
-        const initialGrid = [];
-        for (let row = 0; row < ROW_COUNT; row++){
-            const currentRow = [];
-            for(let col = 0; col < COLUMN_COUNT; col++){
-                currentRow.push(createNode(row, col))
-            }
-            initialGrid.push(currentRow);
-        }
-        return initialGrid;
-    };
-
-    // creates a Node object with the passed in row and col values
-    const createNode = (row , col) => {
-        return {
-            row,
-            col,
-        };
-    };
-
-    // builds and sets the grid value
-    useEffect(() => {
-        const grid = buildInitialGrid();
-        setGrid(grid);
-    }, []);
-
-    // changes the currRow and currCol when a node button is clicked
-    function handleClick (col, row) {
-        setCurrRow(row);
-        setCurrCol(col);
-    };
-
-    // return elements that displays the Coordinates and the Grid 
-    return (
-        <div>
-            <div className="coordinate">Coordinates: (col, row) ({currCol},{currRow})</div>
-                <table className="grid-container" /* grid is put into a table */ >
-                    <tbody className="grid" /* no theader just tbody */>
-                        {
-                        // going through each row in grid
-                        grid.map((row, rowIdx) => {
-                            return (
-                                <tr key={rowIdx}>
-                                    {
-                                    // in each tr it goes through each node(col) in each row of the grid
-                                    row.map((node,nodeIdx) => {
-                                        const {row, col} = node;
-                                        return (
-                                            <Node
-                                                key={nodeIdx}
-                                                col={col}
-                                                row={row}
-                                                onClick={() => handleClick(col, row)}
-                                            ></Node>
-                                        )
-                                    })}
-                              </tr>
-                           )
-                        })}
-                    </tbody>
-                </table>
-            <div></div>
-        </div>
     );
 }
 
