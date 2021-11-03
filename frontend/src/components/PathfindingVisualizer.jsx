e40486ea881c0e53dbf0248edfde6aedc07d5584
David Nikiforov <david.niki4ov97@gmail.com>
2021-11-03 15:19:19 -0400

    pull me_1 branch incorrectly and there was a copies of code in PathfindingVisualizer

--- frontend/src/components/PathfindingVisualizer.jsx
+++ frontend/src/components/PathfindingVisualizer.jsx
@@ -4,25 +4,11 @@ import '../styles/components/grid.css'
 import '../styles/components/node.css'
 import '../styles/layout/coordinate-text.css'
 
-<<<<<<< HEAD
-// Node function that returns a button with id, className, and onClick function
-function Node (props) {
-    const {col, row} = props;
-    return (
-        <button
-            id={`node-${row}-${col}`}
-            className="node"
-            onClick={() => props.onClick()}
-        ></button>
-    );
-}
-=======
 function PathfindingVisualizer() {
 
     // Initializing each constant variable
     const ROW_COUNT = 20;
     const COLUMN_COUNT = 60;
->>>>>>> 4f1304322e96eacf7f09aacd791d94459736680e
 
     // Intializing each state variable
     const [grid, setGrid] = useState([]);
@@ -50,28 +36,6 @@ function PathfindingVisualizer() {
         setCurrRow(row);
         setCurrCol(col);
     };
-
-<<<<<<< HEAD
-    // two callbackFn
-
-    // callbackFn that runs through each row of the grid object
-    function createEachRow (row, rowIdx) {
-        return (
-            <tr key={rowIdx}>
-                {row.map(createColsOfNodesForEachRow)}
-            </tr>
-        );
-    }
-    // callbackFn creates a node for each col in the row
-    function createColsOfNodesForEachRow (node, nodeIdx) {
-        const {row, col} = node;
-        return (
-            <Node key={nodeIdx} col={col} row={row} onClick={() => handleClick(col, row)}/>
-        );
-    }
-
-    // return elements that displays the Coordinates and the Grid 
-=======
     /*
         Itterate throuch every column in the row, insert a Node element into each column
         and then return the results as TableRow elements
@@ -92,23 +56,12 @@ function PathfindingVisualizer() {
     }
 
     // return elements that displays the Coordinates info div and the grid containing all of the Node elements 
->>>>>>> 4f1304322e96eacf7f09aacd791d94459736680e
     return (
         <div className="background">
             {/* 3D button with dark neumorphism */}
             <div className="first-layer">3D Button</div>
             {/* Coordinates outputted and changed after each butto press */}
             <div className="coordinate">Coordinates: (col, row) ({currCol},{currRow})</div>
-<<<<<<< HEAD
-                {/* container for the grid */}
-                <table className="grid-container">
-                    <tbody className="grid">
-                        {/* callbackFn is called for .map on grid */}
-                        {grid.map(createEachRow)}
-                    </tbody>
-                </table>
-            <div></div>
-=======
             <table className="grid-container" /* grid is put into a table */ >
                 <tbody className="grid" /* no theader just tbody */>
                     {
@@ -117,7 +70,6 @@ function PathfindingVisualizer() {
                     }
                 </tbody>
             </table>
->>>>>>> 4f1304322e96eacf7f09aacd791d94459736680e
         </div>
     );
 }
